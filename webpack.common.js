163d38e21d2ccf2fa4fffcc5fdefb7da7408f96a
6ameDev <sumitdas.usit@gmail.com>
2023-03-03 01:12:23 +0530

    Updates index.html

--- webpack.common.js
+++ webpack.common.js
@@ -1,38 +1,57 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    greet: path.join(__dirname, "src/greet.tsx"),
+    index: path.join(__dirname, "src/main.tsx"),
   },
   output: {
     path: path.join(__dirname, "dist/js"),
-    filename: "[name].js"
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader"
+        use: "ts-loader",
       },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader" // Creates style nodes from JS strings
+            loader: "style-loader", // Creates style nodes from JS strings
           },
           {
-            loader: "css-loader" // Translates CSS into CommonJS
+            loader: "css-loader", // Translates CSS into CommonJS
           },
           {
-            loader: "sass-loader" // Compiles Sass to CSS
-          }
-        ]
-      }
-    ]
+            loader: "sass-loader", // Compiles Sass to CSS
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  }
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./node_modules/uikit/dist/css/uikit.min.css",
+          to: path.join(__dirname, "dist/css"),
+        },
+        {
+          from: "./node_modules/uikit/dist/js/uikit.min.js",
+          to: path.join(__dirname, "dist/js"),
+        },
+        {
+          from: "./node_modules/uikit/dist/js/uikit-icons.min.js",
+          to: path.join(__dirname, "dist/js"),
+        }
+      ],
+    }),
+  ],
 };
