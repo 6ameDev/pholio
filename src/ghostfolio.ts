02d69d8879bb42023f4e42384c1acd3d8348f338
6ameDev <sumitdas.usit@gmail.com>
2023-03-03 17:44:19 +0530

    Vested transactions can now be transformed into ghostfolio activities

---
+++ src/ghostfolio.ts
@@ -0,0 +1,51 @@
+export class Ghostfolio {
+  static toTransaction(
+    symbol: string,
+    type: Ghostfolio.Type,
+    fee: number,
+    currency: string,
+    quantity: number,
+    unitPrice: number,
+    dataSource: Ghostfolio.DataSource,
+    date: Date,
+    comment?: string,
+    accountId?: string,
+  ): object {
+    return {
+      symbol,
+      type,
+      fee,
+      currency,
+      quantity,
+      unitPrice,
+      dataSource: resolveDataSource(type, dataSource),
+      date,
+      comment,
+      accountId,
+    };
+  }
+}
+
+function resolveDataSource(type: Ghostfolio.Type, dataSource: Ghostfolio.DataSource) {
+  if (type === Ghostfolio.Type.ITEM) {
+    return Ghostfolio.DataSource.MANUAL;
+  } else {
+    return dataSource;
+  }
+}
+
+export namespace Ghostfolio {
+  export enum Type {
+    BUY = "BUY",
+    SELL = "SELL",
+    DIVIDEND = "DIVIDEND",
+    ITEM = "ITEM",
+  }
+
+  export enum DataSource {
+    YAHOO = "YAHOO",
+    MANUAL = "MANUAL",
+    COINGECKO = "COINGECKO",
+  }
+}
+
--- src/platforms/vested.ts
+++ src/platforms/vested.ts
@@ -1,17 +1,24 @@
+import { Ghostfolio } from "../ghostfolio";
 import Jsun from "../utils/jsun";
 import Str from "../utils/str";
 import Platform from "./platform";
 
-const TXN_RESPONSE_PATH = [
-  "props",
-  "initialReduxState",
-  "transactionHistory",
-  "userTransHistory",
-];
-const RESPONSE_BOUNDS = [
-  '<script id="__NEXT_DATA__" type="application/json">',
-  "</script>",
-];
+const TXN_RESPONSE_PATH = ["props", "initialReduxState", "transactionHistory", "userTransHistory"];
+const RESPONSE_BOUNDS = ['<script id="__NEXT_DATA__" type="application/json">', "</script>"];
+
+const TXN_TYPE_MAP = {
+  SPUR: { type: Ghostfolio.Type.BUY, comment: 'BUY' },
+  SSAL: { type: Ghostfolio.Type.SELL, comment: 'SELL' },
+  DIV: { type: Ghostfolio.Type.DIVIDEND, comment: 'DIVIDEND' },
+  DIVTAX: { type: Ghostfolio.Type.ITEM, comment: 'DIVIDEND TAX' }
+}
+
+const TXN_TYPE_FILTER = (txn) => {
+  const VALID_TXN_TYPES = Object.keys(TXN_TYPE_MAP);
+  return VALID_TXN_TYPES.includes(txn.type);
+};
+
+const CURRENCY = 'USD';
 
 export default class Vested implements Platform {
   name(): string {
@@ -35,12 +42,69 @@ export default class Vested implements Platform {
       const response = this.toJsonResponse(body);
       const txns = Jsun.walk(response, TXN_RESPONSE_PATH);
       if (Array.isArray(txns)) {
-        return { newTxns: txns, latestTxnIndex: -1 };
+        const transformed = this.transformTxns(txns, TXN_TYPE_FILTER);
+        return { newTxns: transformed, latestTxnIndex: -1 };
       }
     } catch (error) {
       console.error(`${this.name()}: Failed to find new txns`);
-      return { newTxns: [], latestTxnIndex: -1 };
     }
+    return { newTxns: [], latestTxnIndex: -1 };
+  }
+
+  private transformTxns(txns: Array<object>, filter: (txn: object) => boolean) {
+    let ignored = new Set();
+    const transformed = txns.reduce((result: any, txn: any) => {
+      if (filter(txn)) {
+        result.push(this.transformTxn(txn))
+      } else {
+        ignored.add(txn.type)
+      }
+      return result;
+    }, []);
+
+    if (ignored.size > 0)
+      console.log(`${this.name()}: Transactions of type ${Array.from(ignored)} were ignored`)
+
+    return transformed;
+  }
+
+  private transformTxn(txn: any, accountId?: string) {
+    return Ghostfolio.toTransaction(
+      txn.symbol,
+      this.resolveType(txn),
+      txn.commission,
+      CURRENCY,
+      this.resolveQuantity(txn),
+      this.resolveUnitPrice(txn),
+      Ghostfolio.DataSource.YAHOO,
+      new Date(txn.date),
+      this.resolveComment(txn),
+      accountId
+    );
+  }
+
+  private resolveQuantity(txn) {
+    const type = this.resolveType(txn);
+    if (type === Ghostfolio.Type.DIVIDEND || type === Ghostfolio.Type.ITEM) {
+      return 1;
+    }
+    return parseFloat(txn.quantity);
+  }
+
+  private resolveUnitPrice(txn) {
+    const type = this.resolveType(txn);
+    if (type === Ghostfolio.Type.DIVIDEND || type === Ghostfolio.Type.ITEM) {
+      return parseFloat(txn.amount);
+    }
+    return parseFloat(txn.fillPrice);
+  }
+
+  private resolveType(txn) {
+    return TXN_TYPE_MAP[txn.type].type
+  }
+
+  private resolveComment(txn) {
+    return TXN_TYPE_MAP[txn.type].comment
   }
 
   private toJsonResponse(body: string): object {
