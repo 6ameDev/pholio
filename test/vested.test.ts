61c76d0618c395cec9f9066a8d703a3f9b4fae36
6ameDev <sumitdas.usit@gmail.com>
2023-03-09 23:26:04 +0530

    refactors vested tests

--- test/vested.test.ts
+++ test/vested.test.ts
@@ -1,9 +1,11 @@
-import { Ghostfolio } from "../src/ghostfolio";
+import { baseMocks } from "./spec_helper";
+import vh from "./vested_helper";
 import Vested from "../src/platforms/vested";
-import { SpecHelper as sh } from "./spec_helper";
+import { Ghostfolio } from "../src/ghostfolio";
 
-const vh = sh.Vested;
-const gen = sh.Vested.TxnGenerator;
+baseMocks();
+const txnGen = vh.TxnGenerator;
+const activityGen = vh.GfActivityGenerator;
 const platform = new Vested();
 
 describe("when platform is vested", () => {
@@ -19,45 +21,67 @@ describe("when platform is vested", () => {
 describe("when platform transactions are ordered new to old", () => {
 
   const TXNS = [
-    gen.dividend("GOOG", "Aug 15, 2022", 20),
-    gen.dividendTax("GOOG", "Aug 15, 2022", 2),
-    gen.sell("META", "Aug 03, 2022", 190, 7),
-    gen.buy("COIN", "Jul 28, 2022", 46, 26),
-    gen.buy("MSFT", "Jul 10, 2022", 260, 0.5),
-    gen.cancelled("ABNB", "Jul 10, 2022", 12),
-    gen.sell("AMZN", "Jul 10, 2022", 150, 15),
-    gen.buy("GOOG", "Jul 05, 2022", 2246, 1),
-    gen.instantFunding("Jul 05, 2022"),
-    gen.buy("QQQM", "Jun 29, 2022", 117, 0.1),
-    gen.buy("BABA", "Jun 29, 2022", 134, 10),
-    gen.rejected("ABNB", "Jun 28, 2022", 43),
-    gen.buy("META", "Jun 28, 2022", 163, 15),
-    gen.buy("AMZN", "Jun 28, 2022", 108, 23),
-    gen.deposit("Jun 28, 2022")
+    txnGen.dividend("GOOG", "Aug 15, 2022", 20),
+    txnGen.dividendTax("GOOG", "Aug 15, 2022", 2),
+    txnGen.sell("META", "Aug 03, 2022", 190, 7),
+    txnGen.buy("COIN", "Jul 28, 2022", 46, 26),
+    txnGen.buy("MSFT", "Jul 10, 2022", 260, 0.5),
+    txnGen.cancelled("ABNB", "Jul 10, 2022", 12),
+    txnGen.sell("AMZN", "Jul 10, 2022", 150, 15),
+    txnGen.buy("GOOG", "Jul 05, 2022", 2246, 1),
+    txnGen.instantFunding("Jul 05, 2022"),
+    txnGen.buy("QQQM", "Jun 29, 2022", 117, 0.1),
+    txnGen.buy("BABA", "Jun 29, 2022", 134, 10),
+    txnGen.rejected("ABNB", "Jun 28, 2022", 43),
+    txnGen.buy("META", "Jun 28, 2022", 163, 15),
+    txnGen.buy("AMZN", "Jun 28, 2022", 108, 23),
+    txnGen.deposit("Jun 28, 2022")
   ]
 
   describe("when last txn does not exist", () => {
     const lastTxn = undefined;
     const accountId = 'random-account-id';
 
-    test("should not return any new txn", () => {
-      const txns = [
-        gen.cancelled("ABNB", "Jul 10, 2022", 12),
-        gen.instantFunding("Jul 05, 2022"),
-        gen.rejected("ABNB", "Jun 28, 2022", 43),
-        gen.deposit("Jun 28, 2022")
-      ];
-      const response = vh.getResponse(txns);
+    describe("when no activity has been done", () => {
+      test("should not return any new txn", () => {
+        const response = vh.getResponse([]);
+  
+        const { newTxns, latestTxnIndex } = platform.findNewTxns(response, lastTxn, accountId);
+        expect(newTxns.length).toStrictEqual(0);
+        expect(latestTxnIndex).toStrictEqual(-1);
+      });
+    })
 
-      const { newTxns, latestTxnIndex } = platform.findNewTxns(response, lastTxn, accountId);
-      expect(newTxns.length).toStrictEqual(0);
-      expect(latestTxnIndex).toStrictEqual(-1);
-    });
+    describe("when only deposit has been made", () => {
+      test("should not return any new txn", () => {
+        const response = vh.getResponse(txnGen.deposit("Jun 28, 2022"));
+  
+        const { newTxns, latestTxnIndex } = platform.findNewTxns(response, lastTxn, accountId);
+        expect(newTxns.length).toStrictEqual(0);
+        expect(latestTxnIndex).toStrictEqual(-1);
+      });
+    })
+
+    describe("when no securities have been bought or sold", () => {
+      test("should not return any new txn", () => {
+        const txns = [
+          txnGen.cancelled("ABNB", "Jul 10, 2022", 12),
+          txnGen.instantFunding("Jul 05, 2022"),
+          txnGen.rejected("ABNB", "Jun 28, 2022", 43),
+          txnGen.deposit("Jun 28, 2022")
+        ];
+        const response = vh.getResponse(txns);
+  
+        const { newTxns, latestTxnIndex } = platform.findNewTxns(response, lastTxn, accountId);
+        expect(newTxns.length).toStrictEqual(0);
+        expect(latestTxnIndex).toStrictEqual(-1);
+      });
+    })
 
     test("should return one new txn", () => {
       const txns = [
-        gen.buy("AMZN", "Jun 28, 2022", 108, 23),
-        gen.deposit("Jun 28, 2022"),
+        txnGen.buy("AMZN", "Jun 28, 2022", 108, 23),
+        txnGen.deposit("Jun 28, 2022"),
       ];
       const response = vh.getResponse(txns);
 
@@ -81,23 +105,20 @@ describe("when platform transactions are ordered new to old", () => {
 
     test("should return all new txns", () => {
       const response = vh.getResponse(TXNS);
-      
-      let res = gen.buy("AMZN", "Jun 28, 2022", 108, 23);
-      let lastTxn = vh.responseToTxn(res, accountId, Type.BUY, "BUY");
+
+      var lastTxn = activityGen.buy("AMZN", "Jun 28, 2022", 108, 23, accountId);
       var { newTxns, latestTxnIndex } = platform.findNewTxns(response, lastTxn, accountId);
       expect(newTxns.length).toStrictEqual(10);
       expect(latestTxnIndex).toStrictEqual(0);
 
-      res = gen.buy("GOOG", "Jul 05, 2022", 2246, 1);
-      lastTxn = vh.responseToTxn(res, accountId, Type.BUY, "BUY");
+      var lastTxn = activityGen.buy("GOOG", "Jul 05, 2022", 2246, 1, accountId);
       var { newTxns, latestTxnIndex } = platform.findNewTxns(response, lastTxn, accountId);
       expect(newTxns.length).toStrictEqual(6);
       expect(latestTxnIndex).toStrictEqual(0);
     });
 
     test("should not return any new txns", () => {
-      const res = gen.dividend("GOOG", "Aug 15, 2022", 20);
-      const lastTxn = vh.responseToTxn(res, accountId, Type.BUY, "BUY");
+      const lastTxn = activityGen.dividend("GOOG", "Aug 15, 2022", 20, accountId);
       const response = vh.getResponse(TXNS);
 
       const { newTxns, latestTxnIndex } = platform.findNewTxns(response, lastTxn, accountId);
@@ -106,8 +127,7 @@ describe("when platform transactions are ordered new to old", () => {
     });
 
     test("should return new txns & latest txn index when they are from same date", () => {
-      const res = gen.sell("META", "Aug 03, 2022", 190, 7);
-      const lastTxn = vh.responseToTxn(res, accountId, Type.SELL, "SELL");
+      const lastTxn = activityGen.sell("META", "Aug 03, 2022", 190, 7, accountId);
       const response = vh.getResponse(TXNS);
 
       const { newTxns, latestTxnIndex } = platform.findNewTxns(response, lastTxn, accountId);
---
+++ test/vested_helper.ts
@@ -0,0 +1,133 @@
+import { Ghostfolio } from "../src/ghostfolio";
+import Meth from "../src/utils/meth";
+
+export default class VestedHelper {
+
+  static getResponse(txn: any): string;
+  static getResponse(txns: any[]): string;
+ 
+  static getResponse(txns: any): string {
+    const transactions = Array.isArray(txns) ? txns : [txns];
+    const txnsStr = JSON.stringify(transactions);
+
+    const RESPONSE_PRE_TXNS = '<script id="__NEXT_DATA__" type="application/json">{"props":{"initialReduxState":{"transactionHistory":{"userTransHistory":'
+    const RESPONSE_POST_TXNS = '}}}}</script>'
+    return RESPONSE_PRE_TXNS.concat(txnsStr, RESPONSE_POST_TXNS);
+  }
+
+  static GfActivityGenerator = {
+    buy: (symbol: string, date: string, fillPrice: number, quantity: number, accountId: string) => {
+      const type = Ghostfolio.Type.BUY;
+      const source = Ghostfolio.DataSource.YAHOO;
+      return Ghostfolio.toTransaction(symbol, type, 0, "USD", quantity, fillPrice, source, new Date(date), "BUY", accountId);
+    },
+
+    sell: (symbol: string, date: string, fillPrice: number, quantity: number, accountId: string) => {
+      const type = Ghostfolio.Type.SELL;
+      const source = Ghostfolio.DataSource.YAHOO;
+      return Ghostfolio.toTransaction(symbol, type, 0, "USD", quantity, fillPrice, source, new Date(date), "SELL", accountId);
+    },
+
+    dividend: (symbol: string, date: string, amount: number, accountId: string) => {
+      const type = Ghostfolio.Type.DIVIDEND;
+      const source = Ghostfolio.DataSource.YAHOO;
+      return Ghostfolio.toTransaction(symbol, type, 0, "USD", 1, amount, source, new Date(date), "DIVIDEND", accountId);
+    },
+
+    item: (symbol: string, date: string, amount: number, accountId: string) => {
+      const type = Ghostfolio.Type.ITEM;
+      const source = Ghostfolio.DataSource.MANUAL;
+      return Ghostfolio.toTransaction(symbol, type, 0, "USD", 1, (-1 * amount), source, new Date(date), "DIVIDEND TAX", accountId);
+    },
+  }
+
+  static TxnGenerator = {
+    buy: (symbol: string, date: string, fillPrice: number, quantity: number) => {
+      const amount = fillPrice * quantity;
+      return {
+        "type": "SPUR",
+        "commission": 0,
+        "date": date,
+        "symbol": symbol,
+        "amount": amount.toString(),
+        "quantity": quantity.toString(),
+        "fillPrice": fillPrice.toString(),
+      }
+    },
+
+    sell: (symbol: string, date: string, fillPrice: number, quantity: number) => {
+      const amount = fillPrice * quantity;
+      return {
+        "type": "SSAL",
+        "commission": 0,
+        "date": date,
+        "symbol": symbol,
+        "amount": amount.toString(),
+        "quantity": quantity.toString(),
+        "fillPrice": fillPrice.toString(),
+      }
+    },
+
+    dividend: (symbol: string, date: string, amount: number) => {
+      return {
+        "type": "DIV",
+        "commission": 0,
+        "symbol": symbol,
+        "date": date,
+        "amount": amount.toString(),
+      }
+    },
+
+    dividendTax: (symbol: string, date: string, amount: number) => {
+      return {
+        "type": "DIVTAX",
+        "commission": 0,
+        "symbol": symbol,
+        "date": date,
+        "amount": (-1 * amount),
+      }
+    },
+
+    cancelled: (symbol: string, date: string, quantity: number) => {
+      return {
+        "type": "ORDER",
+        "orderSide": "BUY",
+        "commission": 0,
+        "amount": null,
+        "symbol": symbol,
+        "date": date,
+        "quantity": quantity,
+      }
+    },
+
+    rejected: (symbol: string, date: string, quantity: number) => {
+      return {
+        "type": "ORDER",
+        "amount": null,
+        "commission": 0,
+        "orderSide": "BUY",
+        "symbol": symbol,
+        "date": date,
+        "quantity": quantity,
+      }
+    },
+
+    instantFunding: (date) => {
+      return {
+        "type": "JNLC",
+        "commission": 0,
+        "amount": Meth.random(1000, 10000),
+        "date": date
+      }
+    },
+
+    deposit: (date) => {
+      return {
+        "type": "CSR",
+        "commission": 0,
+        "amount": Meth.random(1000, 10000),
+        "date": date
+      }
+    }
+  }
+}
