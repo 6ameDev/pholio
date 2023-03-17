0663f0c5f5ecf25a471013704a25d652193c7453
6ameDev <sumitdas.usit@gmail.com>
2023-03-17 16:57:55 +0530

    Minor refactoring & code restructuring

--- src/views/settingsv2/tab.tsx
+++ src/views/settingsv2/tab.tsx
@@ -15,7 +15,7 @@ function a11yProps(index: number) {
 }
 
 export default function SettingsTab() {
-  const [value, setValue] = React.useState(1);
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
