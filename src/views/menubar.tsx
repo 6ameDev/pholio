d5f6d182b849c7a59c9bc09e0c53b851a24c81c7
6ameDev <sumitdas.usit@gmail.com>
2023-03-20 19:43:09 +0530

    Replaces the platforms list with a scrollable platforms list

--- src/views/menubar.tsx
+++ src/views/menubar.tsx
@@ -1,38 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
-import Menu from './settings/menu';
+import SettingsMenu from './settings/menu';
 import { Params as APParams } from './settings/assets_panel';
 import { GhostfolioPanelProps } from './settings/ghostfolio_panel';
 import { PlatformsPanelProps } from './settings/platforms_panel';
+import Platform from '../platforms/platform';
+import { isEqual } from 'lodash';
 
 export interface MenubarProps {
+  platformProps: {
+    platforms: Platform[];
+    currentPlatform: Platform;
+    onClick: (platform: Platform) => void;
+  }
   assetsPanelParams: APParams;
   platformsPanelProps: PlatformsPanelProps;
   ghostfolioPanelProps: GhostfolioPanelProps;
 }
 
 export default function Menubar(props: MenubarProps) {
-  const { assetsPanelParams, platformsPanelProps, ghostfolioPanelProps, ...others } = props;
+  const { platformProps, assetsPanelParams, platformsPanelProps, ghostfolioPanelProps, ...others } = props;
+  const { currentPlatform, platforms, onClick: onPlatformClick } = platformProps;
+
+  const [activePlatform, setActivePlatform] = useState(currentPlatform);
+
+  useEffect(
+    () => {
+      if (typeof activePlatform !== typeof currentPlatform) {
+        setActivePlatform(currentPlatform);
+      }
+    },
+    [currentPlatform]
+  );
+
+  function findPlatformIndex(platforms: Platform[], currentPlatform: Platform) {
+    const index = platforms.findIndex((platform) => {
+      return isEqual(platform, currentPlatform);
+    })
+    return (index > -1) ? index : 0;
+  }
+
+  const handlePlatformClick = (platform: Platform) => {
+    setActivePlatform(platform);
+    onPlatformClick(platform);
+  };
+
+  const tabIndex = findPlatformIndex(platforms, currentPlatform);
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box>
       <AppBar elevation={0} position="static" color="inherit">
         <Toolbar>
           <Typography variant="h6" noWrap component="div">
             Pholio
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
-          <Menu
+          <SettingsMenu
             assetsPanelParams={assetsPanelParams}
             platformsPanelProps={platformsPanelProps}
             ghostfolioPanelProps={ghostfolioPanelProps}
           />
         </Toolbar>
       </AppBar>
+
+      <Typography variant="subtitle1" textAlign="center">PLATFORMS</Typography>
+
+      <Box sx={{ display: 'flex', mb: 5 }}>
+        <Box sx={{ flexGrow: 1 }}></Box>
+        <Tabs value={tabIndex} variant="scrollable"
+              scrollButtons allowScrollButtonsMobile>
+          {platforms.map((platform) => {
+            return (
+              <Tab label={platform.name()} key={platform.name()} onClick={() => handlePlatformClick(platform)} />
+            );
+          })}
+        </Tabs>
+        <Box sx={{ flexGrow: 1 }}></Box>
+      </Box>
     </Box>
   );
 }
